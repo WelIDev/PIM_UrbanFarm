5dd4b98f594f3a9380212863524d17137aec5bc6
Raul Alencar <126506918+raulalen@users.noreply.github.com>
2024-11-16 19:36:20 -0300

    adicao css/js e alguns ajustes html

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5040698e8c3fc42b1a277b5d0c08532a2ff5860")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73f2d96796cf4504c9c732a2e8928c69f749347a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
